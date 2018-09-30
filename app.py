ea9d99eded7df96ea2aae3e59311c0d1f156e223
Wagner Souza <wagnersza@gmail.com>
2018-09-30 09:01:54 -0300

    change def hello return and app.run to bind all ipv4 interfaces

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "########### Hello World ##########"
 
 @app.route("/health")
 def health():
     return "WORKING"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0',port='5000')
